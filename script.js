e718c4934071a2504633536e4821df8b6650bea9
unknown <adewunmiadesokan@gmail.com>
2014-11-25 10:19:47 +0100

    final

--- script.js
+++ script.js
@@ -1,67 +1,81 @@
-var display = document.getElementById("display");
-var user_input = document.getElementById("user-guess");
-var submit = document.getElementsByTagName("button")[0];
-var new_game = document.getElementsByTagName("button")[1];
+var game={  
 
-
-
-var game={
-
- 
+  displays: document.getElementById("display"),
+  user_input: document.getElementById("user-guess"),
+  submit: document.getElementsByTagName("button")[0],
+  new_game: document.getElementsByTagName("button")[1],
+  mercury: document.getElementById("mercury"),
   comp_choice: Math.round(Math.random()*100),
-
   previous_guess: null,
-
   new_guess: "",
 
+  thermometer : function(input){
+  var x = ((100-Math.abs(input-game.comp_choice))*240)/100+ "px";
+  console.log(x);
+  game.mercury.style.width=x; 
+  },  
+
   check_number: function(){
-    this.new_guess=parseInt(user_input.value);
-    console.log(this.new_guess);
-    
+
+    this.new_guess=parseInt(game.user_input.value);
     if (isNaN(this.new_guess) || this.new_guess > 100 || this.new_guess<0 ){
-      display.value= "Not valid, try number between 0 and 100";
+      game.displays.value= "Not valid, try number between 0 and 100";
       return;
     }
     
-    user_input.value="";
+    game.user_input.value="";
       if(game.comp_choice!==this.new_guess) {
-      if(game.previous_guess===null){
-        game.previous_guess=this.new_guess;
-        display.value= "Getting Hotter, try again";
         
-      }
-      else if (this.new_guess===game.previous_guess) {
-        display.value= "Thought I told you that wasn't the number!!";
-      }
-      else{
-        if (Math.abs(this.new_guess-game.comp_choice)<Math.abs(game.previous_guess-game.comp_choice)) {
-          display.value= "Getting Hotter, you are closer";
+        if(game.previous_guess===null){
+          game.previous_guess=this.new_guess;
+          game.displays.value= "Getting Hotter, try again"; 
           
+          game.thermometer(this.new_guess);          
         }
-        else if (Math.abs(this.new_guess-game.comp_choice)>Math.abs(game.previous_guess-game.comp_choice)) {
-          display.value= "Getting Colder, you are getting far";
-         
+
+        else if (this.new_guess===game.previous_guess) {
+          game.displays.value= "Thought I told you that wasn't the number!!";
         }
-        else {
-          display.value= "You are neither Hot nor Cold";
+
+        else{
+
+          if (Math.abs(this.new_guess-game.comp_choice)<Math.abs(game.previous_guess-game.comp_choice)) {
+            game.displays.value= "Getting Hotter, you are closer";
+
+            game.thermometer(this.new_guess);            
+          }
+
+          else if (Math.abs(this.new_guess-game.comp_choice)>Math.abs(game.previous_guess-game.comp_choice)) {
+
+            game.displays.value= "Getting Colder, you are getting far";
+
+            game.thermometer(this.new_guess);                   
+          }
+
+          else {
+            game.displays.value= "You are neither Hot nor Cold";
+          }
+
         }
-      }
       
       game.previous_guess=this.new_guess;
+
     } 
+
     else {
-      display.value= "You got it right";
-      submit.classList.toggle("end-mode");
+      game.displays.value= "You got it right";
+      game.submit.classList.toggle("end-mode");
+
+      game.thermometer(this.new_guess);      
+
     }
+
   }
-
-
 
 };
 
-
-
-
-
 console.log(game.comp_choice);
-submit.addEventListener("click",game.check_number);
+game.submit.addEventListener("click",game.check_number);
+
+
+
